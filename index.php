1e625914cfe9234fbcadef3a3646444bf9848d83
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-04-22 09:00:38 -0700

    Milestone 9 completed

--- index.php
+++ index.php
@@ -6,13 +6,30 @@
 <body>
 	<div class="wrap">
 		<div class="task-list">
-+			<ul>
-+				<?php require("includes/connect.php"); ?>
-+			</ul>
-+		</div>
-+	<form class="add-new-task" autocomplete="off">
-+		<input type="text" name="new-task" placeholder="Add new item..."/>
-+	</form>
-+	</div>	
+			<ul>
+				<?php require("includes/connect.php"); ?>
+			</ul>
+		</div>
+	<form class="add-new-task" autocomplete="off">
+		<input type="text" name="new-task" placeholder="Add new item..."/>
+	</form>
+	</div>	
 </body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task(); //calling the add task function
+	function add_task(){
+		$('.add-new-task').submit(function() {
+			var new_task = $('.add-new-task input[name=new-task]').val();
+
+			if(new_task != ''){
+				$.post('includes/add-task.php', {task: new_task}, function(data){
+					$(('add-new-task input[name=new-task]').val();
+					$(data).appendTo('task-list ul').hide().fadeIn();
+				});
+			}
+			return false;
+		});
+	}
+</script>
 </html>
