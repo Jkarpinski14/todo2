a7a2b9d0be807a7acf968f33c361831cf6bed2bd
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-04-24 17:49:30 -0700

    View files added

---
+++ view/form.php
@@ -0,0 +1,26 @@
+<?php
+    // require the config file to get to the $path variable
+    require_once(__DIR__ . "/../model/config.php");
+    require_once(__DIR__ . "/../controller/login-verify.php");
+    if (!authenticateUser()) {
+        header("Location: " . $path . "index.php");
+        die();
+    }
+?>
+
+<h1>Create Blog Post</h1>
+
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
+    <div>
+        <label for="title">Title: </label>
+        <input type="text" name="title" />
+    </div>
+    
+    <div>
+        <label for="post">Post: </label>
+    </div>
+        
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
